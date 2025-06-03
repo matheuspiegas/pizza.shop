2c8b98d17719cb4c93ee805986ab479c651eb0ca
matheuspiegas <matheusaugustopiegassales@gmail.com>
2025-06-03 11:09:33 -0300

    First commit

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import { defineConfig } from 'eslint/config'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+    },
+  },
+  pluginReact.configs.flat['jsx-runtime'],
+  tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+])
